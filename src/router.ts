b793dbadf8e58312dc7922c2a8d4611924b06d3c
Diego Aguilar <diego.aguilar@kiwicredito.com>
2023-04-29 17:23:31 -0500

    [feat] improve methods

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,21 @@ import { init } from "./application";
 
 const router = express.Router();
 
-router.get('/trengo/webhook', async (req: any, res: any) => {
+router.get('/', async (req: any, res: any) => {
+
+    res.json({
+        message: "v1"
+    });
+});
+
+router.post('/trengo/webhook', async (req: any, res: any) => {
 
     const { message_id, ticket_id, contact_id, message, contact_name, contact_identifier } = req;
 
     const payload = await init({
         message_id, ticket_id, contact_id, message, contact_name, contact_identifier
     })
-    res.send(payload);
+    res.json(payload);
 });
 
 export default router;
