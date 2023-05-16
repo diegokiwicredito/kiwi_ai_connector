7d3ceb1c888ffc1c119caf76440bee2d7c9e2e86
Diego Aguilar <diego.aguilar@kiwicredito.com>
2023-05-15 20:06:13 -0500

    [fix] remove route

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { inbound, outbound, outboundModel, getLoanproUser } from "./application";
+import { inbound, outboundModel, getLoanproUser } from "./application";
 
 const router = express.Router();
 
@@ -18,15 +18,6 @@ router.post('/trengo/webhook/inbound', async (req: any, res: any) => {
     })
     
     res.status(200).json(payload);
-});
-
-router.post('/trengo/webhook/outbound', async (req: any, res: any) => {
-    const { message_id, ticket_id, contact_id, message, contact_name, contact_identifier } = req.body;
-
-    const payload = await outbound({
-        message_id, ticket_id, contact_id, message, contact_name, contact_identifier
-    })
-    res.json(payload);
 });
 
 router.get('/message/:ticket', async (req: any, res: any) => {
